78070d1917f0044304dd79299dfb7bfb79b52ef8
Eliana <eliana4118@gmail.com>
2024-10-18 23:27:34 -0300

    cambios usuario controler

--- src/controllers/usuario.js
+++ src/controllers/usuario.js
@@ -1,14 +1,14 @@
 const { model } = require("mongoose");
 const Usuario = require('../models/usuarios'); // Importa el esquema base
-const Curso = require('../models/cursos'); 
-const { registrarEstudiante, registrarTutor, registrarProfesor, registrarAdministrador,asignarEstudiantesACurso } = require('./funciones/registroRoles');
+const Curso = require('../models/cursos');
+const { registrarEstudiante, registrarTutor, registrarProfesor, registrarAdministrador, asignarEstudiantesACurso } = require('./funciones/registroRoles');
 const UsuarioController = {
   registro: async (req, res) => {
     try {
       const { rol, ...usuarioData } = req.body;
-  
+
       let nuevoUsuario;
-  
+
       switch (rol) {
         case 'estudiante':
           nuevoUsuario = await registrarEstudiante(usuarioData);
@@ -25,7 +25,7 @@ const UsuarioController = {
         default:
           return res.status(400).json({ message: 'Rol no válido' });
       }
-  
+
       return res.status(201).json({ message: 'Usuario registrado exitosamente', usuario: nuevoUsuario });
     } catch (error) {
       console.error('Error al registrar usuario:', error);
@@ -33,7 +33,7 @@ const UsuarioController = {
     }
   },
 
- /* listar2: async (req, res) => {
+  listar: async (req, res) => {
     try {
       const usuarios = await Usuario.find();
       //res.status(200).json(usuarios);
@@ -42,65 +42,65 @@ const UsuarioController = {
     } catch (error) {
       res.status(500).json({ message: 'Error al listar los usuarios', error: error.message });
     }
-  },*/
-
-  listar: async (req, res) => {
-    try {
-      // Obtener los parámetros de los filtros
-      const { nivel, turno, grado, asignado } = req.query;
-      
-      // Crear el objeto de consulta para filtrar solo estudiantes
-      const query = { rol: 'estudiante' };
-  
-      // Aplicar los filtros desde la colección de Usuarios (Nivel, Turno, Grado)
-      if (nivel) {
-        query.nivel = nivel;
-      }
-  
-      if (turno) {
-        query.turno = turno;
-      }
-  
-      if (grado) {
-        query.grado = grado;
-      }
-  
-      // Obtener estudiantes que cumplen con los filtros aplicados
-      let estudiantes = await Usuario.find(query);
-      console.log('Estudiantes encontrados:', estudiantes); 
-  
-      // Filtrar por asignación si se especifica
-      if (asignado) {
-        const cursos = await Curso.find(); // Obtener todos los cursos
-        estudiantes = estudiantes.map(est => {
-          const estaAsignado = cursos.some(curso => curso.estudiantes.includes(est._id));
-          return {
-            ...est.toObject(),
-            asignado: estaAsignado
-          };
-        });
-  
-        // Filtrar por estudiantes asignados o no asignados
-        if (asignado === 'asignados') {
-          estudiantes = estudiantes.filter(est => est.asignado);
-        } else if (asignado === 'noAsignados') {
-          estudiantes = estudiantes.filter(est => !est.asignado);
-        }
-      }
-  
-      console.log('Estudiantes a renderizar:', estudiantes);
-  
-      // Renderizar la vista con los estudiantes filtrados
-      res.render('listar', { estudiantes });
-    } catch (error) {
-      console.error('Error al listar estudiantes:', error);
-      res.status(500).json({ message: 'Error al listar estudiantes' });
-    }
   },
-  
-  
-  
-  
+
+  /*  listar: async (req, res) => {
+     try {
+       // Obtener los parámetros de los filtros
+       const { nivel, turno, grado, asignado } = req.query;
+       
+       // Crear el objeto de consulta para filtrar solo estudiantes
+       const query = { rol: 'estudiante' };
+   
+       // Aplicar los filtros desde la colección de Usuarios (Nivel, Turno, Grado)
+       if (nivel) {
+         query.nivel = nivel;
+       }
+   
+       if (turno) {
+         query.turno = turno;
+       }
+   
+       if (grado) {
+         query.grado = grado;
+       }
+   
+       // Obtener estudiantes que cumplen con los filtros aplicados
+       let estudiantes = await Usuario.find(query);
+       console.log('Estudiantes encontrados:', estudiantes); 
+   
+       // Filtrar por asignación si se especifica
+       if (asignado) {
+         const cursos = await Curso.find(); // Obtener todos los cursos
+         estudiantes = estudiantes.map(est => {
+           const estaAsignado = cursos.some(curso => curso.estudiantes.includes(est._id));
+           return {
+             ...est.toObject(),
+             asignado: estaAsignado
+           };
+         });
+   
+         // Filtrar por estudiantes asignados o no asignados
+         if (asignado === 'asignados') {
+           estudiantes = estudiantes.filter(est => est.asignado);
+         } else if (asignado === 'noAsignados') {
+           estudiantes = estudiantes.filter(est => !est.asignado);
+         }
+       }
+   
+       console.log('Estudiantes a renderizar:', estudiantes);
+   
+       // Renderizar la vista con los estudiantes filtrados
+       res.render('listar', { estudiantes });
+     } catch (error) {
+       console.error('Error al listar estudiantes:', error);
+       res.status(500).json({ message: 'Error al listar estudiantes' });
+     }
+   },
+    */
+
+
+
   /*buscarPorDni: async (req, res) => {
     try {
       const usuario = await Usuario.findOne({ dni: req.params.dni })
@@ -124,18 +124,18 @@ const UsuarioController = {
         .populate('tutores', 'nombres apellidos dni') // Poblar tutores
         .populate('estudiantes', 'nombres apellidos dni') // Poblar estudiantes
         .exec();
-  
+
       // Si el usuario se encuentra
       if (usuario) {
         // Actualizar relaciones si es estudiante y tiene tutores asociados
         if (usuario.rol === 'estudiante' && usuario.dniTutor.length > 0) {
           const tutores = await Usuario.find({ dni: { $in: usuario.dniTutor }, rol: 'tutor' });
-  
+
           // Asignar los tutores al estudiante si no están ya relacionados
           if (!usuario.tutores.length) {
             usuario.tutores = tutores.map(tutor => tutor._id);
             await usuario.save();
-  
+
             // Actualizar los tutores para incluir el estudiante
             await Usuario.updateMany(
               { _id: { $in: tutores.map(tutor => tutor._id) } },
@@ -143,16 +143,16 @@ const UsuarioController = {
             );
           }
         }
-  
+
         // Actualizar relaciones si es tutor y tiene estudiantes asociados
         if (usuario.rol === 'tutor' && usuario.dniEstudiantes.length > 0) {
           const estudiantes = await Usuario.find({ dni: { $in: usuario.dniEstudiantes }, rol: 'estudiante' });
-  
+
           // Asignar los estudiantes al tutor si no están ya relacionados
           if (!usuario.estudiantes.length) {
             usuario.estudiantes = estudiantes.map(estudiante => estudiante._id);
             await usuario.save();
-  
+
             // Actualizar los estudiantes para incluir el tutor
             await Usuario.updateMany(
               { _id: { $in: estudiantes.map(estudiante => estudiante._id) } },
@@ -160,8 +160,8 @@ const UsuarioController = {
             );
           }
         }
-  
-        console.log(usuario);  
+
+        console.log(usuario);
         return res.status(200).json(usuario);
       } else {
         return res.status(404).json({ message: 'Usuario no encontrado' });
@@ -170,9 +170,9 @@ const UsuarioController = {
       return res.status(500).json({ message: 'Error al obtener el usuario', error: error.message });
     }
   },
-  
-  
-  listarPorRol: async (req, res)=> {
+
+
+  listarPorRol: async (req, res) => {
     const { rol } = req.params; // Suponiendo que el rol viene como parámetro en la URL
     try {
       const usuarios = await Usuario.find({ rol }); // Filtra usuarios por rol
@@ -191,26 +191,41 @@ const UsuarioController = {
 
       // Solo agregamos al objeto los campos que están presentes en req.body
       Object.keys(req.body).forEach(campo => {
-          if (req.body[campo] !== undefined) {
-              datosAActualizar[campo] = req.body[campo];
-          }
+        if (req.body[campo] !== undefined) {
+          datosAActualizar[campo] = req.body[campo];
+        }
       });
 
       // Buscamos y actualizamos por DNI
       const usuarioActualizado = await Usuario.findOneAndUpdate(
-          { dni: req.params.dni },
-          { $set: datosAActualizar }, // Solo actualizamos los campos enviados
-          { new: true } // Devolvemos el documento actualizado
+        { dni: req.params.dni },
+        { $set: datosAActualizar }, // Solo actualizamos los campos enviados
+        { new: true } // Devolvemos el documento actualizado
       );
 
       if (!usuarioActualizado) {
-          return res.status(404).json({ message: 'Usuario no encontrado' });
+        return res.status(404).json({ message: 'Usuario no encontrado' });
       }
 
       res.status(200).json({ message: 'Usuario actualizado exitosamente', usuario: usuarioActualizado });
-  } catch (error) {
+    } catch (error) {
       res.status(400).json({ message: 'Error al actualizar el usuario', error: error.message });
-  }
+    }
+  },
+
+  editar: async (req, res) => {
+    try {
+      const usuario = await Usuario.findOne({ dni: req.params.dni });
+
+      if (!usuario) {
+        return res.status(404).render('404', { message: 'Usuario no encontrado' });
+      }
+
+      // Renderiza la vista de edición con los datos del usuario
+      res.render('editarUsuario', { usuario });
+    } catch (error) {
+      res.status(500).send('Error al cargar el formulario de edición');
+    }
   },
 
   eliminar: async (req, res) => {
@@ -227,31 +242,31 @@ const UsuarioController = {
   asignacionCurso: async (req, res) => {
     try {
       const { rol, ...datosAsignacion } = req.body;
-  
+
       let resultado;
-  
+
       switch (rol) {
         case 'estudiante':
           resultado = await asignarEstudiantesACurso(datosAsignacion);
           break;
-  
+
         case 'profesor':
           resultado = await asignarProfesoresACurso(datosAsignacion); // En caso de implementar más adelante
           break;
-  
+
         default:
           return res.status(400).json({ message: 'Rol no válido' });
       }
-  
+
       return res.status(201).json(resultado);
     } catch (error) {
       console.error('Error en la asignación de curso:', error);
       return res.status(400).json({ message: 'Error en la asignación de curso', error: error.message });
     }
   },
-  
-  
-    
+
+
+
 };
 
 
